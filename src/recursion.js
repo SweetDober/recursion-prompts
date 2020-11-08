4c84830d75f884fea9c9982a2a561d15931bc3c2
Anya Glazkova <aglazkova.phd@gmail.com>
2020-11-07 19:45:10 -0800

    Completed isEven function

--- src/recursion.js
+++ src/recursion.js
@@ -64,6 +64,15 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  // allow that negative integers could be passed as args
+  n = Math.abs(n);
+  if(n === 0) {
+    return true;
+  }
+  if(n === 1) {
+    return false;
+  }
+  return (isEven(n - 2));
 };
 
 // 5. Sum all integers below a given integer.
